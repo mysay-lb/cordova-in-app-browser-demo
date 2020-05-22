82901af34303effff0aa9515c79283b0800fbbe6
Mohammad Haidar <mohamad.haidar@mysayapp.com>
2020-05-22 15:18:54 +0300

    re-import in-app browser plugin

--- platforms/ios/platform_www/cordova_plugins.js
+++ platforms/ios/platform_www/cordova_plugins.js
@@ -10,7 +10,7 @@ cordova.define('cordova/plugin_list', function(require, exports, module) {
     }
   ];
   module.exports.metadata = {
-    "cordova-plugin-inappbrowser": "4.0.0-dev",
-    "cordova-plugin-whitelist": "1.3.4"
+    "cordova-plugin-whitelist": "1.3.4",
+    "cordova-plugin-inappbrowser": "4.0.0-dev"
   };
 });
